48164a8e31368f73469ecc715bd5d203b53ff250
nicolascirulli01 <nicolascirulli@gmail.com>
2025-09-03 19:06:10 -0300

    Decorator hecho hasta Nivel 3.1

---
+++ decorator/builder.py
@@ -0,0 +1,28 @@
+from beverages import *
+from condiments import *
+
+def build_beverage(base: str, size: str, condiments: list):
+    
+    bases = {
+        "Espresso": Espresso,
+        "DarkRoast": DarkRoast,
+        "HouseBlend": HouseBlend,
+        "Decaf": Decaf
+    }
+    conds = {
+        "Milk": Milk,
+        "Mocha": Mocha,
+        "Soy": Soy,
+        "Whip": Whip,
+        "Caramel": Caramel
+    }
+    if base not in bases:
+        raise ValueError(f"Bebida base desconocida: {base}")
+    beverage = bases[base]()
+    beverage.set_size(size)
+    # Agregar condimentos
+    for cond in condiments:
+        if cond not in conds:
+            raise ValueError(f"Condimento desconocido: {cond}")
+        beverage = conds[cond](beverage)
+    return beverage
