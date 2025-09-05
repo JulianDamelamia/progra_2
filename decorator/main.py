e14b671859b4839449fe57be85143898f3b06bbf
Damian Piuselli <damianpiuselli@gmail.com>
2025-09-04 22:26:34 -0300

    cambio de nombre a archivos para adoptar convencion de pytest

--- decorator/main.py
+++ decorator/main.py
@@ -6,6 +6,7 @@ from condiments import Mocha, Whip, Soy, Caramel, Milk
 from builder import build_beverage
 from prettyPrintDecorator import PrettyPrintDecorator
 
+
 def main():
     """
     Función principal que simula la preparación de cafés en Starbuzz.
@@ -22,7 +23,7 @@ def main():
     beverage2 = DarkRoast()
     beverage2 = Mocha(beverage2)  # Envolvemos con el primer Mocha
     beverage2 = Mocha(beverage2)  # Envolvemos con el segundo Mocha
-    beverage2 = Whip(beverage2)   # Envolvemos con Crema
+    beverage2 = Whip(beverage2)  # Envolvemos con Crema
     beverage2 = PrettyPrintDecorator(beverage2)  # Aplicamos el decorador pretty print
     print(f"Pedido 2: {beverage2.get_description()} ${beverage2.cost():.2f}")
 
@@ -43,7 +44,7 @@ def main():
     beverage4 = PrettyPrintDecorator(beverage4)
     print(f"Pedido 4: {beverage4.get_description()} ${beverage4.cost():.2f}")
 
-    #Pedido 5: Un Decaf Grande con leche y soja
+    # Pedido 5: Un Decaf Grande con leche y soja
     beverage5 = DarkRoast()
     beverage5.set_size("Grande")
     beverage5 = Milk(beverage5)
@@ -51,7 +52,7 @@ def main():
     beverage5 = PrettyPrintDecorator(beverage5)
     print(f"Pedido 5: {beverage5.get_description()} ${beverage5.cost():.2f}")
 
-    #Pedido 6: Un HouseBLend Venti con soja
+    # Pedido 6: Un HouseBLend Venti con soja
     beverage6 = HouseBlend()
     beverage6.set_size("Venti")
     beverage6 = Soy(beverage6)
@@ -66,6 +67,7 @@ def main():
     beverage8 = build_beverage("DarkRoast", "Tall", ["Mocha", "Mocha", "Whip"])
     beverage8 = PrettyPrintDecorator(beverage8)
     print(f"Pedido 8: {beverage8.get_description()} ${beverage8.cost():.2f}")
+
 
 if __name__ == "__main__":
     main()
