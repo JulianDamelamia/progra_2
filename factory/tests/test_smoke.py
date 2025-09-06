1af6a5c4159a2299a020193e08c098291c53b1bb
JulianDamelamia <jlfraga@estudiantes.unsam.edu.ar>
2025-09-06 14:28:51 -0300

    arreglo en la definición de las clases (mal indentado) y agrego un if __name__==__main__ en test

--- factory/tests/test_smoke.py
+++ factory/tests/test_smoke.py
@@ -1,5 +1,14 @@
+import sys
+import os
+
+# Add the parent directory of 'simple_factory', 'factory_method', and 'abstract_factory' to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 def test_imports():
-    import factory.simple_factory.main as s
-    import factory.factory_method.main as fm
-    import factory.abstract_factory.main as af
+    import simple_factory.main as s
+    import factory_method.main as fm
+    import abstract_factory.main as af
     assert callable(s.main) and callable(fm.main) and callable(af.main)
+
+if __name__ == "__main__":
+    test_imports()
